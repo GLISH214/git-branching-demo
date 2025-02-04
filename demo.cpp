1f1705da18998650e6ec38fca8687759a083f065
LISHITA <138212264+GLISH214@users.noreply.github.com>
2025-02-04 09:47:29 +0530

    Modified demo.cpp with new message

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,23 @@
-// demo.cpp - Simple Git branching demo
 #include <iostream>
 using namespace std;
 
+// Function to print out the Git commands
+void printGitCommands() {
+    cout << "- Forking a repository" << endl;
+    cout << "- Creating a new branch" << endl;
+    cout << "- Committing changes" << endl;
+    cout << "- Pushing to GitHub" << endl;
+    cout << "- Opening a Pull Request" << endl;
+}
+
 int main() {
-    cout << "Hello, Git Branching!" << endl;
-    cout << "This is for practice!" << endl;
+    cout << "Welcome to the Updated Git Branching Demo!" << endl;
+    cout << "This is a modified C++ program for learning Git branching." << endl;
+    
+    // Display the steps in the demo
+    cout << "In this demo, we will showcase the following Git steps:" << endl;
+    printGitCommands();
+    
+    cout << "Thank you for following along and learning Git with us!" << endl;
     return 0;
 }
